b2be465881c9c6cc93c402d9651cbc8b02249467
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2021-06-21 23:40:34 +0300

    Ogrenci.cpp

--- Week1/Sample2/src/Ogrenci.cpp
+++ Week1/Sample2/src/Ogrenci.cpp
@@ -23,3 +23,14 @@ double Ogrenci::getBoy(){
 Ogrenci::~Ogrenci(){
   	cout<<"Cagrildi"<<endl;
 }
+Ogrenci::Ogrenci(int ys,double by,double kl){
+	yas=ys;
+	boy=by;
+	kilo=kl;
+}
+double Ogrenci::getKilo(){
+	return kilo;
+}
+void Ogrenci::YemekYe(Sebze yemek){
+	kilo += yemek.getKalori()/1000;
+}
