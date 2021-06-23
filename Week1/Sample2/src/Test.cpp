da5a5e1b6d43f6e83d732826ded6ba00e63ebd01
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2021-06-23 15:50:20 +0300

    Test.cpp

--- Week1/Sample2/src/Test.cpp
+++ Week1/Sample2/src/Test.cpp
@@ -1,16 +1,22 @@
 #include "Ogrenci.hpp"
+#include "Sebze.hpp"
+#include "Meyve.hpp"
 
 int main(){
-	Ogrenci o1(5,135,52);	
+	Ogrenci o1(5,135,52);
+	
 	cout<<"Ogrenci :"<<endl;
 	cout<<"Yas :"<<o1.getYas()<<endl;	
 	cout<<"Boy :"<<o1.getBoy()<<endl;	
 	cout<<"Kilo :"<<o1.getKilo()<<endl;
 	Sebze fasulye(1200);
-	o1.YemekYe(fasulye);
+	o1.YemekYe<Sebze>(fasulye);
+	Meyve kavun(2000);
+	o1.YemekYe<Meyve>(kavun);
 	cout<<"Ogrenci :"<<endl;
 	cout<<"Yas :"<<o1.getYas()<<endl;	
 	cout<<"Boy :"<<o1.getBoy()<<endl;	
 	cout<<"Kilo :"<<o1.getKilo()<<endl;
 	return 0;
 }
+
