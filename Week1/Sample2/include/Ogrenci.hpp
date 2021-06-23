1c0d72731ee647a58441fc8fd4de8b6c8be8047c
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2021-06-23 15:48:36 +0300

    Ogrenci.hpp

--- Week1/Sample2/include/Ogrenci.hpp
+++ Week1/Sample2/include/Ogrenci.hpp
@@ -10,15 +10,40 @@ class Ogrenci{
 		double boy;
 		double kilo;
 	public:
-		Ogrenci(int,double,double);
-		Ogrenci();
-		void YasIlerle(int);
-		void BoyUza(double);
-		int getYas();
-		double getBoy();
-		void yemekYe(Sebze);
-		double getKilo()const; 
-		~Ogrenci();
+		Ogrenci(int ys,double by){
+			yas=ys;
+			boy=by;
+		}
+		Ogrenci(){
+			yas=0;
+			boy=30;
+		}
+		void YasIlerle(int ys){
+			yas+=ys;
+		}
+		void BoyUza(double by){
+			boy+=by;
+		}
+		int getYas(){
+			return yas;
+		}
+		double getBoy(){
+			return boy;
+		}
+		~Ogrenci(){
+			cout<<"Cagrildi"<<endl;
+		}
+		Ogrenci(int ys,double by,double kl){
+			yas=ys;
+			boy=by;
+			kilo=kl;
+		}
+		double getKilo(){
+			return kilo;
+		}
+		void YemekYe(Sebze yemek){
+			kilo += yemek.getKalori()/1000;
+		}
 };
 
 #endif
