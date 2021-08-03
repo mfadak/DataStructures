56f6bb6b92d278640dda559127f876cba338748f
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2021-08-03 23:05:59 +0300

    Test.cpp

---
+++ Week5/LinkedList/src/Test.cpp
@@ -0,0 +1,100 @@
+#pragma GCC diagnostic ignored "-Wdeprecated"
+#include <cstdlib>
+#include "LinkedList.hpp"
+
+int main()
+{
+	LinkedList<string> *isimler = new LinkedList<string>();
+	int sec=0;
+	string isim;
+	int konum;
+	
+	do{
+		system("CLS");
+		cout<<"1.Isim Ekle"<<endl;
+		cout<<"2.Isim Sil"<<endl;
+		cout<<"3.Konumdan Isim Sil"<<endl;
+		cout<<"4.Konuma Isim Ekle"<<endl;
+		cout<<"5.Konum Bul"<<endl;
+		cout<<"6.Isim Sayisi"<<endl;
+		cout<<"7.Ilk Isim"<<endl;
+		cout<<"8.Son Isim"<<endl;
+		cout<<"9.Isimleri Listele"<<endl;
+		cout<<"10.Isimleri Temizle"<<endl;
+		cout<<"11.Cikis"<<endl;
+		cout<<"Secim:";
+		cin>>sec;
+		system("CLS");
+		switch(sec){
+			case 1:
+				cout<<"Isim:";
+				cin.ignore();
+				getline(cin,isim);
+				isimler->add(isim);
+			break;
+			case 2:
+				cout<<"Isim:";
+				cin.ignore();
+				getline(cin,isim);
+				isimler->remove(isim);
+			break;
+			case 3:
+					cout<<"Konum:";
+					cin>>konum;
+					isimler->removeAt(konum);
+			break;
+			case 4:
+					cout<<"Konum:";
+					cin>>konum;
+					cout<<"Isim:";
+					cin.ignore();
+					getline(cin,isim);
+					isimler->insert(konum,isim);
+			break;
+			case 5:
+					cout<<"Isim:";
+					cin.ignore();
+					getline(cin,isim);
+					cout<<"Konum:"<<isimler->indexOf(isim);
+					cin.ignore();
+					cin.get();
+			break;
+			case 6:			
+				cout<<"Isim Sayisi:"<<isimler->Count()<<endl;
+				cin.ignore();
+				cin.get();
+			break;	
+			case 7:
+					cout<<"Ilk Isim:"<<isimler->first()<<endl;
+					cin.ignore();
+					cin.get();
+			break;	
+			case 8:
+					cout<<"Son Isim:"<<isimler->last()<<endl;
+					cin.ignore();
+					cin.get();
+			break;	
+			case 9:			
+				cout<<"ISIMLER:"<<endl;
+				cout<<*isimler<<endl;
+				cin.ignore();
+				cin.get();
+			break;	
+			case 10:
+				isimler->clear();
+				cout<<endl<<"Tum isimler silindi."<<endl;
+				cin.ignore();
+				cin.get();
+			break;
+			case 11:
+			break;
+			default:
+				cout<<endl<<"Yanlis tercih!"<<endl;
+				cin.ignore();
+				cin.get();
+			break;
+		}
+		system("CLS");
+	}while(sec != 11);
+	delete isimler;
+}
