a34f3970feb2f3e5d96ed8c503c5f865e76b8ce8
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2021-09-05 20:42:57 +0300

    Create Test.cpp

---
+++ Week7/ArrayBSTTree/src/Test.cpp
@@ -0,0 +1,34 @@
+#include "BST.hpp"
+
+int main(){
+	BST<int> *tree = new BST<int>();
+	
+	tree->add(50);
+	tree->add(40);
+	tree->add(60);
+	tree->add(30);
+	tree->add(45);
+	tree->add(55);
+	tree->add(90);
+	tree->add(15);
+	tree->add(35);
+	tree->add(42);
+	tree->add(48);
+	tree->add(54);
+	tree->add(56);
+	tree->add(70);
+	tree->add(100);
+	
+	cout<<"Height: "<<tree->Height()<<endl;
+	
+	if(tree->find(35)) cout<<"35 is exist"<<endl;
+	if(tree->find(500)) cout<<"500 is exist"<<endl;
+	else cout<<"500 is not exist"<<endl;
+	
+	tree->postorder();
+	tree->Delete(50);
+	cout<<endl;
+	tree->inorder();
+	delete tree;
+	return 0;
+}
