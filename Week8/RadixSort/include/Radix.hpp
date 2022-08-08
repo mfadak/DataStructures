4fead781f7a0c512aee233f6ad95abb5e3114e9b
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-08-08 20:47:58 +0300

    Radix.hpp

---
+++ Week8/RadixSort/include/Radix.hpp
@@ -0,0 +1,22 @@
+#ifndef RADIX_HPP
+#define RADIX_HPP
+
+#include "Queue.hpp"
+#include <cmath>
+
+class Radix{
+	private:	
+		int* numbers;
+		int length;
+		Queue<int> **queues;
+		int maxStep;
+		
+		int MaxStepNumber();
+		int StepCount(int);
+	public:
+		Radix(int*, int);
+		int* Sort();
+		~Radix();
+};
+
+#endif
