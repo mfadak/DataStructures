efe84346bcdc54e26e2dbd48d14c651e5b839e61
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-12-10 10:50:16 +0300

    Test.cpp

--- Week12/HeapTree/src/Test.cpp
+++ Week12/HeapTree/src/Test.cpp
@@ -1,26 +1,23 @@
-#include "HeapTree.hpp"
 #include <cstdlib>
+#include "HeapTree.hpp"
 
 int main(){
-	HeapTree *heapAgaci = new HeapTree(100);
-	int *dizi = new int[25];
-	cout<<"Dizi ilk Hali:"<<endl;
+	int *array = new int[25];
+	cout<<"Array before ordered:"<<endl;
 	for(int i=0;i<25;i++){
-		dizi[i] = rand()%100+1;
-		heapAgaci->Add(dizi[i]);
-		cout<<dizi[i]<<" ";
+		array[i] = rand()%100+1;
+		cout<<array[i]<<" ";
 	}
-	int index=0;
-	while(!heapAgaci->IsEmpty()){
-		dizi[index] = heapAgaci->RemoveMin();
-		index++;
-	}
-	cout<<endl<<"Dizinin Siralanmis Hali:"<<endl;
+	
+	HeapTree *heap = new HeapTree(array,25);
+	delete [] array;
+	array = heap->Sort();
+	cout<<endl<<"Ordered Array:"<<endl;
 	for(int i=0;i<25;i++){
-		cout<<dizi[i]<<" ";
+		cout<<array[i]<<" ";
 	}
 	cout<<endl;
-	delete [] dizi;
-	delete heapAgaci;
+	delete [] array;
+	delete heap;
 	return 0;
 }
