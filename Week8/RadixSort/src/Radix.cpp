60057c45b59bf72b4938fbe29b641a1a1b38c241
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2023-07-25 16:22:43 +0300

    Update Radix.cpp

--- Week8/RadixSort/src/Radix.cpp
+++ Week8/RadixSort/src/Radix.cpp
@@ -1,76 +1,44 @@
 #include "Radix.hpp"
 
-int Radix::MaxStepNumber(){
-	int max=0;
-	for(int i=0;i<length;i++){
-		if(StepCount(numbers[i])>max) max = StepCount(numbers[i]);
-	}
-	return max;
-}
-int Radix::StepCount(int number){
-	number = abs(number);
-	int basamak=0;
-	while(number>0){
-		basamak++;
-		number /= 10;
-	}
-	return basamak;
-}
 Radix::Radix(int *numbers, int length){
 	this->numbers = new int[length];
 	this->length = length;
 	for(int i=0;i<length;i++){ this->numbers[i] = numbers[i]; }
 	queues = new Queue<int>*[10];
 	for(int j=0;j<10;j++){ queues[j] = new Queue<int>(); }
-	maxStep = MaxStepNumber();
 }
-int* Radix::QueueCurrentLengths(){
-	int *lengths = new int[10];
-	for(int i=0;i<10;i++){
-		lengths[i] = queues[i]->count();
-	}
-	return lengths;
-}
-int Radix::power(int e){
-	int result=1;
-	for(int i=0;i<e;i++) result *= 10;
-	return result;
+int Radix::findMax() {
+    int max = numbers[0];
+    for (int i = 1; i < length; ++i) {
+        if (numbers[i] > max)
+            max = numbers[i];
+    }
+    return max;
 }
 int* Radix::Sort(){
-	int numberIndex=0;
-	// read from array once and add to queues
-	for(;numberIndex<length;numberIndex++){
-		int stepValue = numbers[numberIndex]%10;
-		int num = numbers[numberIndex];
-		queues[stepValue]->enqueue(num);
-	}
-		
-	//i starting from 1 because of first digit processed
-	for(int i=1;i<maxStep;i++){
-		//get the current length in all queues
-		int *qlengths = QueueCurrentLengths();
-		for(int index=0;index<10;index++){
-			int len=qlengths[index];
-			for(;len>0;len--){
-				int number = queues[index]->peek();
-				queues[index]->dequeue();
-				int stepValue = (number/power(i))%10;			
-				queues[stepValue]->enqueue(number);				
-			}
-		}
-		delete [] qlengths;
-	}
-	int* ordered = new int[length];
-	numberIndex=0;
-	for(int index=0;index<10;index++){
-		while(!queues[index]->isEmpty()){
-			int number = queues[index]->peek();
-			ordered[numberIndex++] = number;
-			queues[index]->dequeue();
-		}
-	}
-		
-	return ordered;
+	int max = findMax();
+    int exp = 1; // basamak yeri
+
+   while (max / exp > 0) {
+        // Basamak değerine göre ilgili kuyruğa ata
+        for (int i=0;i<length;i++) {
+            int digit = (numbers[i] / exp) % 10;
+            queues[digit]->enqueue(numbers[i]);
+        }
+
+        // Kuyruktan sayıları alıp diziyi güncelleştir.
+        int index = 0;
+        for (int i = 0; i < 10; ++i) {
+            while (!queues[i]->isEmpty()) {
+                numbers[index++] = queues[i]->peek();
+                queues[i]->dequeue();
+            }
+        }
+
+        // Diğer basamak değerine ilerle
+        exp *= 10;
+    }
+	return numbers;
 }
 Radix::~Radix(){	
 	delete [] numbers;
